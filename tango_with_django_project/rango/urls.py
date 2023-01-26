916efcbbf5c2ad518c8d4d19071e14cf30df7a5f
Ahmed Alonaizi <2721819a@student.gla.ac.uk>
2023-01-26 14:07:07 +0000

    ok

---
+++ tango_with_django_project/rango/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 20 09:35:26 2023
+
+@author: bullz
+"""
+
+from django.urls import path
+from rango import views
+app_name = 'rango'
+
+
+urlpatterns = [
+path('', views.index, name='index'),
+]
